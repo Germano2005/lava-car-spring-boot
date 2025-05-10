85b5248ed15b1efc69f81bc40e13e064fa1e8030
Germano2005 <163420182+Germano2005@users.noreply.github.com>
2025-05-10 17:24:53 -0300

    Criando o Front-end da aplicação

--- src/main/java/br/edu/ifsc/fln/lavacarspringboot/models/Cor.java
+++ src/main/java/br/edu/ifsc/fln/lavacarspringboot/models/Cor.java
@@ -1,10 +1,7 @@
 package br.edu.ifsc.fln.lavacarspringboot.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
